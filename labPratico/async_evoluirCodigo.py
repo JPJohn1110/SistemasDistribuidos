e356f5ee1be6fa8488d6690cd33f20e1f2683c0e
João Pedro Santos <jpsr.jpsantos@gmail.com>
2024-12-03 22:33:15 -0300

    fix: atualização do problema 2

--- labPratico/async_evoluirCodigo.py
+++ labPratico/async_evoluirCodigo.py
@@ -1,24 +1,31 @@
 import asyncio
+import random
 
 async def task(id, segs):
-	#Executa algo que não dependa de você…
-	#p.ex: Replicação de DB em server secundário
 	print(f"Task{id} Iniciada")
 	await asyncio.sleep(segs)
 	print(f"Task{id} Concluída finalizada em {segs} segundas")
 
-async def main():
-	print('Aplicação Iniciada')
-	t1 = asyncio.create_task(task(1,5))
-	t2 = asyncio.create_task(task(2,8))
-	t3 = asyncio.create_task(task(3,3))
+async def main_primeira():
+
+	t = [asyncio.create_task(task(i, random.randint(3,10))) for i in range(30)]
 
 	done, pending = await asyncio.wait(
-		{t1, t2,t3}, 
+		t, 
 		return_when=asyncio.FIRST_COMPLETED
-  	)
-	
+	)
+
 	for i in done:
 		await i
+async def main_todas():
 
-asyncio.run(main())
+	t = [asyncio.create_task(task(i, random.randint(3,10))) for i in range(30)]
+
+	done, pending = await asyncio.wait(
+		t, 
+		return_when=asyncio.ALL_COMPLETED
+	)
+
+	for i in done:
+		await i
+asyncio.run(main_todas())
